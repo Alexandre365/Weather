5a98620c64f3ca83dfff2345fe40687f00b620b9
Alexandre <tube998@gmail.com>
2023-03-06 21:36:34 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,43 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import { FaMapMarkerAlt, FaSearch } from 'react-icons/fa';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [NameCity, setNameCity] = useState('City');
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="Container">
+      <div className="Box">
+        <div className="Banner">
+          <div className="Title">
+            <FaMapMarkerAlt />
+            <div>
+              <h2>{NameCity}</h2>
+              <p></p>
+            </div>
+          </div>
+          <div
+            className="Input"
+            onKeyUp={e => {
+              if (e.key === 'Enter') {
+                console.log();
+              }
+            }}
+          >
+            <div className="Input_Main">
+              <input
+                type="text"
+                placeholder="Search..."
+                onChange={e => {
+                  console.log();
+                }}
+              />
+              <div className="Btn_icon">
+                <FaSearch />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
